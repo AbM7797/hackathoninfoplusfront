6ae76dcd39b4432beb3975b88a22e5c9897314e0
Marwen FRAJ <marwen.fraj1@esprit.tn>
2021-03-07 04:01:29 +0100

    added single page

--- src/app/examples/landing/landing.component.ts
+++ src/app/examples/landing/landing.component.ts
@@ -9,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class LandingComponent implements OnInit {
   focus: any;
   focus1: any;
+  isImageAvailable : boolean = true;
+  isVideoAvailable : boolean = false;
 
   constructor() { }
 
-  ngOnInit() {}
-
+  ngOnInit() {
+    console.log('this.isImageAvailable' + this.isImageAvailable)
+   console.log('isVideoAvailable' + this.isVideoAvailable)
+  }
+  onChangeToVideo(){
+    this.isImageAvailable = false;
+    this.isVideoAvailable = true;
+  }
+  onVideoToImage(){
+    this.isImageAvailable = true;
+    this.isVideoAvailable = false;
+  }
 }
